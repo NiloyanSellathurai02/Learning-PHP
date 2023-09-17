369c54454cdc44cd8bf7db347150c793152b1e27
NiloyanSellathurai02 <n.sella2001@gmail.com>
2023-09-17 21:45:40 +0200

    Learned About nexting arrays

--- Arrays/index.php
+++ Arrays/index.php
@@ -10,13 +10,20 @@
 
     <?php 
         $books = [
-            "Do Androids Dream of Electric Sheep" , 
-            "The Langoliers",
-            "Hail Mary" 
+           [
+            "name" =>'Do Androids Dream of Electric Sheep',
+            "author" => 'Philip K. Dick',
+            "purchaseUrl" => 'http://example.com',
+           ],
+           [
+            "name" =>'Project Hail Mary',
+            "author" => 'Andy Weir',
+            "purchaseUrl" => 'http://example.com',
+           ]
         ];
     ?>
     <ul>
-        <!-- Manier 1 -->
+        <!-- Manier 1
         <?php foreach ($books as $book) : ?>
         
             <li><?= $book ?></li>
@@ -28,7 +35,9 @@
        <?php foreach ($books as $book){
         echo "<li>$book</li>";
        }
-       ?>
+       ?> -->
     </ul>
+
+    
 </body>
 </html>
