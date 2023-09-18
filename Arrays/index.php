92aa206c493d653a4f78ef899963abfa1d4f9939
NiloyanSellathurai02 <n.sella2001@gmail.com>
2023-09-18 18:36:40 +0200

    Learned About creating Functions and how to filter

--- Arrays/index.php
+++ Arrays/index.php
@@ -11,33 +11,68 @@
     <?php 
         $books = [
            [
-            "name" =>'Do Androids Dream of Electric Sheep',
-            "author" => 'Philip K. Dick',
+            "name" =>'Do Androids Dream of Electric Sheep', // Associative Array
+            "author" => 'Philip K. Dick', // Give the properties a key value
             "purchaseUrl" => 'http://example.com',
+            "releaseDate" => 2021,
            ],
            [
             "name" =>'Project Hail Mary',
             "author" => 'Andy Weir',
-            "purchaseUrl" => 'http://example.com',
+            "purchaseUrl" => 'http://google.nl',
+            "releaseDate" => 2017,
+           ],
+           [
+            "name" =>'Javascript Killer',
+            "author" => 'Andy Weir',
+            "purchaseUrl" => 'http://yahoo.com',
+            "releaseDate" => 2023,
            ]
         ];
+
+        function filterByAuthor($books,$author) {
+            $filteredBook = [];
+
+            foreach($books as $book){
+                if($book['author'] === $author){
+                    $filteredBook[] = $book;
+                }
+            }
+
+            return $filteredBook;
+        }
+
+
+
     ?>
     <ul>
         <!-- Manier 1
         <?php foreach ($books as $book) : ?>
         
-            <li><?= $book ?></li>
+            <li></li>
         
         <?php endforeach; ?>
             <br>
             
             <!-- Manier 2 -->
        <?php foreach ($books as $book){
-        echo "<li>$book</li>";
+        // echo "<li>$book</li>";
        }
-       ?> -->
+       ?>
     </ul>
-
+    <ul>
+        <?php foreach (filterByAuthor($books, 'Philip K. Dick') as $book):?>
+            
+                <li>
+                <a href="<?= $book['purchaseUrl']?>">
+                        <?=$book['name']?> (<?=$book['releaseDate']?>)
+                    </a>
+                </li>
+           
+        <?php endforeach; ?>    
+    </ul>
+    <p>
+    </p>
     
 </body>
 </html>
